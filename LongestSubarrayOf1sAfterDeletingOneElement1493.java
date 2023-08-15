a86dff2c9d6205eccf59565029abfced1c8c0b62
Tuan Le <tlna95@icloud.com>
2023-08-15 00:39:48 -0400

    I will come back for the 20th...

---
+++ LongestSubarrayOf1sAfterDeletingOneElement1493.java
@@ -0,0 +1,26 @@
+/*
+Given a binary array nums, you should delete one element from it.
+Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
+*/
+public class LongestSubarrayOf1sAfterDeletingOneElement1493 {
+    public static void main(String[] args) {
+        System.out.println(longestSubarray(new int[]{1, 1, 0, 1}));
+        System.out.println(longestSubarray(new int[]{0, 1, 1, 1, 0, 1, 1, 0, 1}));
+        System.out.println(longestSubarray(new int[]{1, 1, 1}));
+    }
+
+    public static int longestSubarray(int[] nums) {
+        int longest = 0;
+        int deleted = 0;
+        int start = 0;
+        for (int i = 0; i < nums.length; i++) {
+            deleted += (nums[i] == 0 ? 1 : 0);
+            while (deleted > 1) {
+                deleted -= (nums[start] == 0 ? 1 : 0);
+                start++;
+            }
+            longest = Math.max(longest, i - start);
+        }
+        return longest;
+    }
+}
