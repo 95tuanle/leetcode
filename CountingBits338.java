a95a47dae45c0afb40d65d562e16bc7c49e44618
Tuan Le <tlna95@icloud.com>
2023-09-01 16:08:36 -0400

    40th, not very great

---
+++ CountingBits338.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+/*
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+*/
+public class CountingBits338 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(countBits(5)));
+    }
+
+    public static int[] countBits(int n) {
+        int[] integers = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            integers[i] = Integer.bitCount(i);
+        }
+        return integers;
+    }
+}
