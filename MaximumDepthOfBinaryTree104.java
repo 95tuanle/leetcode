ecee6bd177f0951f3dd970129ccfc551c76fc8fd
Tuan Le <tlna95@icloud.com>
2023-09-15 13:57:28 -0400

    43rd

--- MaximumDepthOfBinaryTree104.java
+++ MaximumDepthOfBinaryTree104.java
@@ -11,7 +11,7 @@ public class MaximumDepthOfBinaryTree104 {
     }
 
     public static int maxDepth(TreeNode root) {
-        return 0;
+        return root == null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
 
     public static class TreeNode {
