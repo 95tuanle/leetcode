1eacadf43c2d39b5b3991ad790b8c5baa73e8128
Tuan Le <tlna95@icloud.com>
2023-09-21 02:43:20 -0400

    not very optimized

--- MedianOfTwoSortedArrays4.java
+++ MedianOfTwoSortedArrays4.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
 Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 The overall run time complexity should be O(log (m+n)).
@@ -9,6 +11,22 @@ public class MedianOfTwoSortedArrays4 {
     }
 
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        return 0;
+        int length = nums1.length + nums2.length;
+        int[] merged = new int[length];
+        int i = 0;
+        for (int j : nums1) {
+            merged[i] = j;
+            i++;
+        }
+        for (int j : nums2) {
+            merged[i] = j;
+            i++;
+        }
+        Arrays.sort(merged);
+        if (length % 2 == 1) {
+            return merged[length / 2];
+        } else {
+            return (double) (merged[length / 2 - 1] + merged[length / 2]) / 2;
+        }
     }
 }
