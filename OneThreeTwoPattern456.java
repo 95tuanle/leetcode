1c2c772b402b8e9bea604ee09f0b487a6e00d175
Tuan Le <tlna95@icloud.com>
2023-09-30 09:39:28 -0400

    add skeleton

---
+++ OneThreeTwoPattern456.java
@@ -0,0 +1,15 @@
+/*
+Given an array of n integers nums, a 132 pattern is a subsequence of three integers nums[i], nums[j] and nums[k] such that i < j < k and nums[i] < nums[k] < nums[j].
+Return true if there is a 132 pattern in nums, otherwise, return false.
+*/
+public class OneThreeTwoPattern456 {
+    public static void main(String[] args) {
+        System.out.println(find132pattern(new int[]{1, 2, 3, 4}));
+        System.out.println(find132pattern(new int[]{3, 1, 4, 2}));
+        System.out.println(find132pattern(new int[]{-1, 3, 2, 0}));
+    }
+
+    public static boolean find132pattern(int[] nums) {
+        return false;
+    }
+}
