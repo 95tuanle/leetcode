0fac965dbafc77d1f733d3f22c59a25d05970c6a
Tuan Le <tlna95@icloud.com>
2023-09-30 10:00:32 -0400

    needs more optimization

--- OneThreeTwoPattern456.java
+++ OneThreeTwoPattern456.java
@@ -10,6 +10,15 @@ public class OneThreeTwoPattern456 {
     }
 
     public static boolean find132pattern(int[] nums) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] < nums[k] && nums[k] < nums[j]) {
+                        return true;
+                    }
+                }
+            }
+        }
         return false;
     }
 }
