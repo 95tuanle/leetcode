f4f3127c5b2e96cf734eb83be15d9fd46808cb4f
Tuan Le <tlna95@icloud.com>
2023-10-05 15:51:41 -0400

    bada bing

--- MaximumLevelSumOfABinaryTree1161.java
+++ MaximumLevelSumOfABinaryTree1161.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /*
 Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
 Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
@@ -12,8 +17,50 @@ public class MaximumLevelSumOfABinaryTree1161 {
     }
 
     public static int maxLevelSum(TreeNode root) {
-        return 0;
+        List<TreeNode> list = new ArrayList<>();
+        list.add(root);
+        return maxLevelSum(list, root.val, 1, 1);
     }
+
+    private static int maxLevelSum(List<TreeNode> list, int maxSum, int maxLevel, int currentLevel) {
+        if (list.isEmpty()) return maxLevel;
+        int sum = 0;
+        List<TreeNode> nextLevel = new ArrayList<>();
+        for (TreeNode node : list) {
+            sum += node.val;
+            if (node.left != null) nextLevel.add(node.left);
+            if (node.right != null) nextLevel.add(node.right);
+        }
+        if (sum > maxSum) {
+            maxSum = sum;
+            maxLevel = currentLevel;
+        }
+        return maxLevelSum(nextLevel, maxSum, maxLevel, currentLevel + 1);
+    }
+
+//    public static int maxLevelSum(TreeNode root) {
+//        if (root == null) return 0;
+//        int maxSum = root.val, maxLevel = 1, currentLevel = 1;
+//        Queue<TreeNode> queue = new LinkedList<>();
+//        queue.offer(root);
+//        while (!queue.isEmpty()) {
+//            int sizeOfLevel = queue.size();
+//            int sum = 0;
+//            for (int i = 0; i < sizeOfLevel; i++) {
+//                TreeNode node = queue.poll();
+//                assert node != null;
+//                sum += node.val;
+//                if (node.left != null) queue.offer(node.left);
+//                if (node.right != null) queue.offer(node.right);
+//            }
+//            if (sum > maxSum) {
+//                maxSum = sum;
+//                maxLevel = currentLevel;
+//            }
+//            currentLevel++;
+//        }
+//        return maxLevel;
+//    }
 
     public static class TreeNode {
         int val;
