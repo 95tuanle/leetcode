91d6eddb24af15e934f705be0a1ca476b17a7b2e
95tuanle <tlna95@icloud.com>
2024-01-04 03:57:18 -0500

    😬

---
+++ BinaryTreeInorderTraversal94.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+*/
+public class BinaryTreeInorderTraversal94 {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode right = new TreeNode(2);
+        root.right = right;
+        right.left = new TreeNode(3);
+        System.out.println(inorderTraversal(root));
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        depthFirstSearch(root, result);
+        return result;
+    }
+
+    public static void depthFirstSearch(TreeNode root, List<Integer> result) {
+        if (root == null) return;
+        depthFirstSearch(root.left, result);
+        result.add(root.val);
+        depthFirstSearch(root.right, result);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
