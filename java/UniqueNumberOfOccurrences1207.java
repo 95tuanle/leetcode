adf626dbb876a8d0396f4aaf14614dc7c50bc6ec
95tuanle <tlna95@icloud.com>
2024-01-16 22:38:21 -0500

    tidy up

--- java/UniqueNumberOfOccurrences1207.java
+++ java/UniqueNumberOfOccurrences1207.java
@@ -1,7 +1,5 @@
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 
 /*
 Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
@@ -14,9 +12,9 @@ public class UniqueNumberOfOccurrences1207 {
     }
 
     public static boolean uniqueOccurrences(int[] arr) {
-        Map<Integer, Integer> freq = new HashMap<>();
+        HashMap<Integer, Integer> freq = new HashMap<>();
         for (int num : arr) freq.put(num, freq.getOrDefault(num, 0) + 1);
-        Set<Integer> freqSet = new HashSet<>(freq.values());
+        HashSet<Integer> freqSet = new HashSet<>(freq.values());
         return freq.size() == freqSet.size();
 
 //        HashMap<Integer, Integer> dict = new HashMap<>();
