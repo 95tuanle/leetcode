2db7dc0877d97cb43b0c8826c56a97ae10e31147
95tuanle <tlna95@icloud.com>
2024-01-20 02:46:00 -0500

    smh

--- java/SumOfSubarrayMinimums907.java
+++ java/SumOfSubarrayMinimums907.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Stack;
+
 /*
 Given an array of integers arr, find the sum of min(b), where b ranges over every (contiguous) subarray of arr. Since the answer may be large, return the answer modulo 109 + 7.
 */
@@ -11,6 +14,21 @@ public class SumOfSubarrayMinimums907 {
     }
 
     public static int sumSubarrayMins(int[] arr) {
-        return 0;
+        Stack<Integer> stack = new Stack<>();
+        int[] previousLesser = new int[arr.length];
+        int[] nextLesserOrEqual = new int[arr.length];
+        Arrays.fill(nextLesserOrEqual, arr.length);
+        for (int i = 0; i < arr.length; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] >= arr[i]) nextLesserOrEqual[stack.pop()] = i;
+            previousLesser[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        long sum = 0;
+        double mod = Math.pow(10, 9) + 7;
+        for (int i = 0; i < arr.length; i++) {
+            sum += (long) (i - previousLesser[i]) * (nextLesserOrEqual[i] - i) * arr[i];
+            sum %= (long) mod;
+        }
+        return (int) sum;
     }
 }
