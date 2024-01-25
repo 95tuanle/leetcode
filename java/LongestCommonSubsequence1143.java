00e5689dd1bcdec31a1af963de1e53a567df6fb7
95tuanle <tlna95@icloud.com>
2024-01-25 09:21:55 -0500

    skeletonize

---
+++ java/LongestCommonSubsequence1143.java
@@ -0,0 +1,17 @@
+/*
+Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+For example, "ace" is a subsequence of "abcde".
+A common subsequence of two strings is a subsequence that is common to both strings.
+*/
+public class LongestCommonSubsequence1143 {
+    public static void main(String[] args) {
+        System.out.println(longestCommonSubsequence("abcde", "ace"));
+        System.out.println(longestCommonSubsequence("abc", "abc"));
+        System.out.println(longestCommonSubsequence("abc", "def"));
+    }
+
+    public static int longestCommonSubsequence(String text1, String text2) {
+        return 0;
+    }
+}
