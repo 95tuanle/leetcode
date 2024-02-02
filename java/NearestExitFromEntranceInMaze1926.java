0ed6c2c1f8ac4088e19d8dab08e7a2a1f2fb8ea0
95tuanle <tlna95@icloud.com>
2024-02-02 10:59:18 -0500

    finally

--- java/NearestExitFromEntranceInMaze1926.java
+++ java/NearestExitFromEntranceInMaze1926.java
@@ -21,23 +21,84 @@ public class NearestExitFromEntranceInMaze1926 {
     }
 
     public static int nearestExit(char[][] maze, int[] entrance) {
+        maze[entrance[0]][entrance[1]] = '+';
         ArrayList<int[]> neighbors = new ArrayList<>();
-        if (entrance[0] - 1 >= 0)
-            if (maze[entrance[0] - 1][entrance[1]] == '.') neighbors.add(new int[]{entrance[0] - 1, entrance[1]});
-        if (entrance[1] + 1 < maze[0].length)
-            if (maze[entrance[0]][entrance[1] + 1] == '.') neighbors.add(new int[]{entrance[0], entrance[1] + 1});
-        if (entrance[0] + 1 < maze.length)
-            if (maze[entrance[0] + 1][entrance[1]] == '.') neighbors.add(new int[]{entrance[0] + 1, entrance[1]});
-        if (entrance[1] - 1 >= 0)
-            if (maze[entrance[0]][entrance[1] - 1] == '.') neighbors.add(new int[]{entrance[0], entrance[1] - 1});
+        if (entrance[0] - 1 >= 0) if (maze[entrance[0] - 1][entrance[1]] == '.') {
+            neighbors.add(new int[]{entrance[0] - 1, entrance[1]});
+            maze[entrance[0] - 1][entrance[1]] = '+';
+        }
+        if (entrance[1] + 1 < maze[0].length) if (maze[entrance[0]][entrance[1] + 1] == '.') {
+            neighbors.add(new int[]{entrance[0], entrance[1] + 1});
+            maze[entrance[0]][entrance[1] + 1] = '+';
+        }
+        if (entrance[0] + 1 < maze.length) if (maze[entrance[0] + 1][entrance[1]] == '.') {
+            neighbors.add(new int[]{entrance[0] + 1, entrance[1]});
+            maze[entrance[0] + 1][entrance[1]] = '+';
+        }
+        if (entrance[1] - 1 >= 0) if (maze[entrance[0]][entrance[1] - 1] == '.') {
+            neighbors.add(new int[]{entrance[0], entrance[1] - 1});
+            maze[entrance[0]][entrance[1] - 1] = '+';
+        }
         if (neighbors.isEmpty()) return -1;
+        return breathFirstSearch(maze, neighbors, 1);
 
-        return 0;
+//        int m = maze.length, n = maze[0].length;
+//        Queue<int[]> queue = new LinkedList<>();
+//        queue.offer(entrance);
+//        maze[entrance[0]][entrance[1]] = '+';
+//        int steps = 0;
+//        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+//        while (!queue.isEmpty()) {
+//            int size = queue.size();
+//            for (int i = 0; i < size; i++) {
+//                int[] current = queue.poll();
+//                assert current != null;
+//                int x = current[0], y = current[1];
+//                if ((x == 0 || x == m - 1 || y == 0 || y == n - 1) && (x != entrance[0] || y != entrance[1]))
+//                    return steps;
+//                for (int[] direction : directions) {
+//                    int newX = x + direction[0];
+//                    int newY = y + direction[1];
+//                    if (newX >= 0 && newX < m && newY >= 0 && newY < n && maze[newX][newY] == '.') {
+//                        queue.offer(new int[]{newX, newY});
+//                        maze[newX][newY] = '+';
+//                    }
+//                }
+//            }
+//            steps++;
+//        }
+//        return -1;
     }
 
-    private static ArrayList<int[]> findNeighbors(char[][] maze, int[] entrance) {
-        ArrayList<int[]> neighbors = new ArrayList<>();
-
-        return neighbors;
+    private static int breathFirstSearch(char[][] maze, ArrayList<int[]> neighbors, int currentLayer) {
+        ArrayList<int[]> nextNeighbors = new ArrayList<>();
+        for (int[] neighbor : neighbors) {
+            if (neighbor[0] - 1 >= 0) {
+                if (maze[neighbor[0] - 1][neighbor[1]] == '.') {
+                    nextNeighbors.add(new int[]{neighbor[0] - 1, neighbor[1]});
+                    maze[neighbor[0] - 1][neighbor[1]] = '+';
+                }
+            } else return currentLayer;
+            if (neighbor[1] + 1 < maze[0].length) {
+                if (maze[neighbor[0]][neighbor[1] + 1] == '.') {
+                    nextNeighbors.add(new int[]{neighbor[0], neighbor[1] + 1});
+                    maze[neighbor[0]][neighbor[1] + 1] = '+';
+                }
+            } else return currentLayer;
+            if (neighbor[0] + 1 < maze.length) {
+                if (maze[neighbor[0] + 1][neighbor[1]] == '.') {
+                    nextNeighbors.add(new int[]{neighbor[0] + 1, neighbor[1]});
+                    maze[neighbor[0] + 1][neighbor[1]] = '+';
+                }
+            } else return currentLayer;
+            if (neighbor[1] - 1 >= 0) {
+                if (maze[neighbor[0]][neighbor[1] - 1] == '.') {
+                    nextNeighbors.add(new int[]{neighbor[0], neighbor[1] - 1});
+                    maze[neighbor[0]][neighbor[1] - 1] = '+';
+                }
+            } else return currentLayer;
+        }
+        if (nextNeighbors.isEmpty()) return -1;
+        return breathFirstSearch(maze, nextNeighbors, currentLayer + 1);
     }
 }
