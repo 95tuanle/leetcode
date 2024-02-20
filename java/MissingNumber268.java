d1ccdbd91f0d26dc8261629e732185aae6d875c0
95tuanle <tlna95@icloud.com>
2024-02-19 22:22:04 -0500

    better solution

--- java/MissingNumber268.java
+++ java/MissingNumber268.java
@@ -10,18 +10,18 @@ public class MissingNumber268 {
     }
 
     public static int missingNumber(int[] nums) {
-        int n = nums.length;
-        int sum = n * (n + 1) / 2;
-        for (int num : nums) {
-            sum -= num;
+//        int n = nums.length;
+//        int sum = n * (n + 1) / 2;
+//        for (int num : nums) {
+//            sum -= num;
+//        }
+//        return sum;
+
+        int sum = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            sum -= nums[i];
+            sum += i;
         }
         return sum;
-
-//        int currentSum = 0, expectedSum = nums.length;
-//        for (int i = 0; i < nums.length; i++) {
-//            currentSum += nums[i];
-//            expectedSum += i;
-//        }
-//        return expectedSum - currentSum;
     }
 }
