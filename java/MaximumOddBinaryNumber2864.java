0ff8a36a09ceeeab70b43b1892cf987d16015f36
95tuanle <tlna95@icloud.com>
2024-03-01 01:41:48 -0500

    hmmm

--- java/MaximumOddBinaryNumber2864.java
+++ java/MaximumOddBinaryNumber2864.java
@@ -12,16 +12,31 @@ public class MaximumOddBinaryNumber2864 {
         System.out.println(maximumOddBinaryNumber("111"));
         System.out.println(maximumOddBinaryNumber("10000000000000000000000000000000"));
         System.out.println(maximumOddBinaryNumber("10000000000000000000000000000001"));
+        System.out.println(maximumOddBinaryNumber("1"));
     }
 
     public static String maximumOddBinaryNumber(String s) {
-        int oneCounter = 0;
-        for (char c : s.toCharArray()) if (c == '1') oneCounter++;
-        StringBuilder result = new StringBuilder(s.length());
-        for (int i = 0; i < s.length() - 1; i++)
-            if (i < oneCounter - 1) result.append('1');
-            else result.append('0');
-        result.append('1');
-        return result.toString();
+        char[] bits = s.toCharArray();
+        int left = 0, right = bits.length - 1;
+        while (left <= right) {
+            if (bits[left] == '1') left++;
+            if (bits[right] == '0') right--;
+            if (left <= right && bits[left] == '0' && bits[right] == '1') {
+                bits[left] = '1';
+                bits[right] = '0';
+            }
+        }
+        bits[left - 1] = '0';
+        bits[bits.length - 1] = '1';
+        return new String(bits);
+
+//        int oneCounter = 0;
+//        for (char c : s.toCharArray()) if (c == '1') oneCounter++;
+//        StringBuilder result = new StringBuilder(s.length());
+//        for (int i = 0; i < s.length() - 1; i++)
+//            if (i < oneCounter - 1) result.append('1');
+//            else result.append('0');
+//        result.append('1');
+//        return result.toString();
     }
 }
