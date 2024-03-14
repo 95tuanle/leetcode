6ca8c4674aefd7933995f10bcb1bef46998c748d
95tuanle <tlna95@icloud.com>
2024-03-14 01:00:17 -0400

    F

--- java/BinarySubarraysWithSum930.java
+++ java/BinarySubarraysWithSum930.java
@@ -1,5 +1,3 @@
-import java.util.HashMap;
-
 /*
 Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
 A subarray is a contiguous part of the array.
@@ -16,15 +14,16 @@ public class BinarySubarraysWithSum930 {
     }
 
     public static int numSubarraysWithSum(int[] nums, int goal) {
-        int result = 0, sum = 0;
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for (int num : nums) {
-            sum += num;
-            if (sum == goal) result++;
-            if (hashMap.containsKey(sum - goal)) result += hashMap.get(sum - goal);
-            hashMap.put(sum, hashMap.getOrDefault(sum, 0) + 1);
-        }
-        return result;
+        return slidingWindowAtMost(nums, goal) - slidingWindowAtMost(nums, goal - 1);
+//        int result = 0, sum = 0;
+//        HashMap<Integer, Integer> hashMap = new HashMap<>();
+//        for (int num : nums) {
+//            sum += num;
+//            if (sum == goal) result++;
+//            if (hashMap.containsKey(sum - goal)) result += hashMap.get(sum - goal);
+//            hashMap.put(sum, hashMap.getOrDefault(sum, 0) + 1);
+//        }
+//        return result;
 
 //        int result = 0;
 //        for (int i = goal == 0 ? 1 : goal; i <= nums.length; i++) {
@@ -38,5 +37,25 @@ public class BinarySubarraysWithSum930 {
 //            }
 //        }
 //        return result;
+    }
+
+    private static int slidingWindowAtMost(int[] nums, int goal) {
+        if (goal < 0) return 0;
+        int result = 0, start = 0, end = 0, sum = 0;
+        while (end < nums.length) {
+            sum += nums[end++];
+            while (sum > goal) sum -= nums[start++];
+            result += end - start;
+        }
+        return result;
+
+//        if (goal < 0) return 0;
+//        int start = 0, currentSum = 0, totalCount = 0;
+//        for (int end = 0; end < nums.length; end++) {
+//            currentSum += nums[end];
+//            while (currentSum > goal && start <= end) currentSum -= nums[start++];
+//            totalCount += end - start + 1;
+//        }
+//        return totalCount;
     }
 }
