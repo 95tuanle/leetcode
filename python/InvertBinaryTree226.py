72b61467c32d20cd2f2f8541a34a9ce2583c4895
95tuanle <tlna95@icloud.com>
2024-03-15 13:28:43 -0400

    add an additional approach

--- python/InvertBinaryTree226.py
+++ python/InvertBinaryTree226.py
@@ -15,6 +15,15 @@ def invert_tree(root: Optional[TreeNode]) -> Optional[TreeNode]:
     root.left, root.right = invert_tree(root.right), invert_tree(root.left)
     return root
 
+    # if root:
+    #     stack = [root]
+    #     while stack:
+    #         node = stack.pop()
+    #         if node:
+    #             node.left, node.right = node.right, node.left
+    #             stack += node.left, node.right
+    # return root
+
 
 def print_tree(root: Optional[TreeNode], indent: str = ''):
     if not root:
