6467f4ee96d4a1cc1831d34b6fd3257c9c96bd00
95tuanle <tlna95@icloud.com>
2024-03-14 22:15:42 -0400

    update the solution

--- java/ProductOfArrayExceptSelf238.java
+++ java/ProductOfArrayExceptSelf238.java
@@ -12,19 +12,18 @@ public class ProductOfArrayExceptSelf238 {
     }
 
     public static int[] productExceptSelf(int[] nums) {
-        int[] result = new int[nums.length];
-        int product = 1;
-        for (int i = 0; i < nums.length; i++) {
+        int product = 1, i = 0, length = nums.length;
+        int[] result = new int[length];
+        for (; i < length; i++) {
             result[i] = product;
             product *= nums[i];
         }
-
         product = 1;
-        for (int i = nums.length - 1; i >= 0; i--) {
+        i--;
+        for (; i >= 0; i--) {
             result[i] *= product;
             product *= nums[i];
         }
-
         return result;
 
 //        int[] result = new int[nums.length];
