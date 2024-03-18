e94b052ed26db2c17b9c7e62c517747cd87c113d
95tuanle <tlna95@icloud.com>
2024-03-18 12:38:26 -0400

    finalize java

--- java/NthTribonacciNumber1137.java
+++ java/NthTribonacciNumber1137.java
@@ -12,14 +12,25 @@ public class NthTribonacciNumber1137 {
     public static int tribonacci(int n) {
         if (n == 0) return 0;
         if (n == 1 || n == 2) return 1;
-        int[] dp = new int[n + 1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
+        int a = 0, b = 1, c = 1, temp;
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+            temp = c;
+            c += a + b;
+            a = b;
+            b = temp;
         }
-        return dp[n];
+        return c;
+
+//        if (n == 0) return 0;
+//        if (n == 1 || n == 2) return 1;
+//        int[] dp = new int[n + 1];
+//        dp[0] = 0;
+//        dp[1] = 1;
+//        dp[2] = 1;
+//        for (int i = 3; i <= n; i++) {
+//            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+//        }
+//        return dp[n];
 
 //        int a = 0, b = 1, c = 1;
 //        for (int i = 3; i <= n + 2; i++) {
