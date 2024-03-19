9cddeeb2a42149d0d968809635444f63cc9494e0
95tuanle <tlna95@icloud.com>
2024-03-19 02:19:09 -0400

    optimize

--- python/TaskScheduler621.py
+++ python/TaskScheduler621.py
@@ -2,27 +2,31 @@
 # interval allows the completion of one task. Tasks can be completed in any order, but there's a constraint:
 # identical tasks must be separated by at least n intervals due to cooling time. Return the minimum number of
 # intervals required to complete all tasks.
-from collections import Counter, deque
-from queue import PriorityQueue
+from collections import Counter
 from typing import List
 
 
 def leastInterval(tasks: List[str], n: int) -> int:
     task_counts = Counter(tasks)
-    pq = PriorityQueue()
-    for count in task_counts.values():
-        pq.put(-count)
-    time = 0
-    q = deque()
-    while not pq.empty() or len(q) != 0:
-        time += 1
-        if not pq.empty():
-            count = pq.get() + 1
-            if count < 0:
-                q.append([count, time + n])
-        if len(q) != 0 and q[0][1] == time:
-            pq.put(q.popleft()[0])
-    return time
+    max_count = max(task_counts.values())
+    max_count_tasks = sum(1 for count in task_counts.values() if count == max_count)
+    return max(len(tasks), (max_count - 1) * (n + 1) + max_count_tasks)
+
+    # task_counts = Counter(tasks)
+    # pq = PriorityQueue()
+    # for count in task_counts.values():
+    #     pq.put(-count)
+    # time = 0
+    # q = deque()
+    # while not pq.empty() or len(q) != 0:
+    #     time += 1
+    #     if not pq.empty():
+    #         count = pq.get() + 1
+    #         if count < 0:
+    #             q.append([count, time + n])
+    #     if len(q) != 0 and q[0][1] == time:
+    #         pq.put(q.popleft()[0])
+    # return time
 
 
 if __name__ == "__main__":
