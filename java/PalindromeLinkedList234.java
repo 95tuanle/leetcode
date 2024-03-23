af38594f7f953fc84e99c5ff13daa51fb0571ddb
95tuanle <tlna95@icloud.com>
2024-03-23 00:22:42 -0400

    optimize java

--- java/PalindromeLinkedList234.java
+++ java/PalindromeLinkedList234.java
@@ -3,18 +3,26 @@ Given the head of a singly linked list, return true if it is a palindrome or fal
 */
 public class PalindromeLinkedList234 {
     public static void main(String[] args) {
-        ListNode head = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
+//        1 2 3 3 2 1
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3, new ListNode(2, new ListNode(1))))));
         System.out.println(isPalindrome(head));
 
         head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(2, new ListNode(1)))));
         System.out.println(isPalindrome(head));
+
+        head = new ListNode(1, new ListNode(2, new ListNode(2, new ListNode(1))));
+        System.out.println(isPalindrome(head));
+
+        head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        System.out.println(isPalindrome(head));
     }
 
     public static boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
         ListNode slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            fast = fast.next.next;
+        while (fast.next != null && fast.next.next != null) {
             slow = slow.next;
+            fast = fast.next.next;
         }
         ListNode end = null, next;
         while (slow != null) {
