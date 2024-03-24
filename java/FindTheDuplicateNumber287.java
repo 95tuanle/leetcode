00dc5c8d68aacc0cac334731ac189ee2617f2ed6
95tuanle <tlna95@icloud.com>
2024-03-24 01:27:40 -0400

    update Java solution

--- java/FindTheDuplicateNumber287.java
+++ java/FindTheDuplicateNumber287.java
@@ -10,13 +10,11 @@ public class FindTheDuplicateNumber287 {
     }
 
     public static int findDuplicate(int[] nums) {
-        int slow = nums[0];
-        int fast = nums[0];
+        int slow = nums[0], fast = nums[0];
         do {
             slow = nums[slow];
             fast = nums[nums[fast]];
         } while (slow != fast);
-        System.out.println();
         slow = nums[0];
         while (slow != fast) {
             slow = nums[slow];
