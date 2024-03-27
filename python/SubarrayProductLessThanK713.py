1dd900cc81f3b4cee397e7a3b2e84b1dd2aaac14
95tuanle <tlna95@icloud.com>
2024-03-27 04:04:12 -0400

    add python solution

---
+++ python/SubarrayProductLessThanK713.py
@@ -0,0 +1,24 @@
+# Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of
+# all the elements in the subarray is strictly less than k.
+from typing import List
+
+
+def numSubarrayProductLessThanK(nums: List[int], k: int) -> int:
+    counter = 0
+    if len(nums) == 0:
+        return 0
+    product, left = 1, 0
+    for right in range(len(nums)):
+        product *= nums[right]
+        while left <= right and product >= k:
+            product //= nums[left]
+            left += 1
+        counter += right - left + 1
+    return counter
+
+
+if __name__ == "__main__":
+    print(numSubarrayProductLessThanK([10, 5, 2, 6], 100))
+    print(numSubarrayProductLessThanK([1, 2, 3], 0))
+    print(numSubarrayProductLessThanK([1, 1, 1], 1))
+    print("The values above should be 8, 0, and 0.")
