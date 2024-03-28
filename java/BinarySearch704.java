8e3a2f25164dfc7d71fbfe54285b5c6483f8a114
95tuanle <tlna95@icloud.com>
2024-03-28 19:46:58 -0400

    update solution

--- java/BinarySearch704.java
+++ java/BinarySearch704.java
@@ -11,14 +11,11 @@ public class BinarySearch704 {
     public static int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1, mid;
         while (left <= right) {
-            mid = left + (right - left) / 2;
+            mid = (right + left) / 2;
             if (nums[mid] == target) return mid;
             else if (target < nums[mid]) right = mid - 1;
             else left = mid + 1;
         }
         return -1;
-
-//        for (int i = 0; i < nums.length; i++) if (nums[i] == target) return i;
-//        return -1;
     }
 }
