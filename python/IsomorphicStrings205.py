9d6adeb5709020c196e2ed2d192c89df17d4005b
95tuanle <tlna95@icloud.com>
2024-04-02 00:41:36 -0400

    add python

---
+++ python/IsomorphicStrings205.py
@@ -0,0 +1,22 @@
+# Given two strings s and t, determine if they are isomorphic. Two strings s and t are isomorphic if the characters
+# in s can be replaced to get t. All occurrences of a character must be replaced with another character while
+# preserving the order of characters. No two characters may map to the same character, but a character may map to
+# itself.
+def isIsomorphic(s: str, t: str) -> bool:
+    s_map, t_map = {}, {}
+    for i in range(len(s)):
+        s_char, t_char = s[i], t[i]
+        if s_map.get(s_char, t_char) != t_char or t_map.get(t_char, s_char) != s_char:
+            return False
+        s_map[s_char] = t_char
+        t_map[t_char] = s_char
+    return True
+
+
+if __name__ == "__main__":
+    print(isIsomorphic("egg", "add"))  # True
+    print(isIsomorphic("foo", "bar"))  # False
+    print(isIsomorphic("paper", "title"))  # True
+    print(isIsomorphic("badc", "baba"))  # False
+    print(isIsomorphic("ab", "aa"))  # False
+    print(isIsomorphic("ab", "ca"))  # True
