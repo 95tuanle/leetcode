490072bfd9d87f6bd335b95aa17e31ae5fd8fc03
95tuanle <tlna95@icloud.com>
2024-04-03 01:30:02 -0400

    add description

--- java/WordSearch79.java
+++ java/WordSearch79.java
@@ -1,3 +1,7 @@
+/*
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+*/
 public class WordSearch79 {
     public static void main(String[] args) {
         char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
