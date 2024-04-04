a4344cba79808ee5d67e2ca6cc5495ed4cf12494
95tuanle <tlna95@icloud.com>
2024-04-04 19:03:19 -0400

    skeletonize the question

---
+++ java/DominoAndTrominoTiling790.java
@@ -0,0 +1,23 @@
+/*
+You have two types of tiles: a 2 x 1 domino shape and a tromino shape. You may rotate these shapes.
+Given an integer n, return the number of ways to tile an 2 x n board. Since the answer may be very large, return it modulo 109 + 7.
+In a tiling, every square must be covered by a tile. Two tilings are different if and only if there are two 4-directionally adjacent cells on the board such that exactly one of the tilings has both squares occupied by a tile.
+*/
+public class DominoAndTrominoTiling790 {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(numTilings(n));
+
+        n = 4;
+        System.out.println(numTilings(n));
+
+        n = 5;
+        System.out.println(numTilings(n));
+    }
+
+    public static int numTilings(int n) {
+        int result = 0;
+
+        return result;
+    }
+}
