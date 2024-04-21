bbf333eeeb951d6984a6d0e018f54144734d33f9
95tuanle <tlna95@icloud.com>
2024-04-21 00:54:40 -0400

    add DFS solution

---
+++ java/FindIfPathExistsInGraph1971.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/*
+There is a bi-directional graph with n vertices, where each vertex is labeled from 0 to n - 1 (inclusive). The edges in the graph are represented as a 2D integer array edges, where each edges[i] = [ui, vi] denotes a bi-directional edge between vertex ui and vertex vi. Every vertex pair is connected by at most one edge, and no vertex has an edge to itself.
+You want to determine if there is a valid path that exists from vertex source to vertex destination.
+Given edges and the integers n, source, and destination, return true if there is a valid path from source to destination, or false otherwise.
+*/
+public class FindIfPathExistsInGraph1971 {
+    public static void main(String[] args) {
+        System.out.println(validPath(3, new int[][]{{0, 1}, {1, 2}, {2, 0}}, 0, 2)); // true
+        System.out.println(validPath(6, new int[][]{{0, 1}, {0, 2}, {3, 5}, {5, 4}, {4, 3}}, 0, 5)); // false
+    }
+
+    public static boolean validPath(int n, int[][] edges, int source, int destination) {
+        HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) graph.put(i, new ArrayList<>());
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        boolean[] visited = new boolean[n];
+        return depthFirstSearch(source, destination, graph, visited);
+    }
+
+    private static boolean depthFirstSearch(int source, int destination, HashMap<Integer, ArrayList<Integer>> graph, boolean[] visited) {
+        if (source == destination) return true;
+        visited[source] = true;
+        for (int neighbor : graph.get(source))
+            if (!visited[neighbor] && depthFirstSearch(neighbor, destination, graph, visited)) return true;
+        return false;
+    }
+}
