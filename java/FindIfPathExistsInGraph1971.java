d1199b55b0ae5f7351bbf0523fde1de8a9f60455
95tuanle <tlna95@icloud.com>
2024-04-29 14:12:56 -0400

    add union find java solution

--- java/FindIfPathExistsInGraph1971.java
+++ java/FindIfPathExistsInGraph1971.java
@@ -1,7 +1,3 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-
 /*
 There is a bidirectional graph with n vertices, where each vertex is labeled from 0 to n - 1 (inclusive). The edges in the graph are represented as a 2D integer array edges, where each edges[i] = [ui, vi] denotes a bi-directional edge between vertex ui and vertex vi. Every vertex pair is connected by at most one edge, and no vertex has an edge to itself.
 You want to determine if there is a valid path that exists from vertex source to vertex destination.
@@ -13,26 +9,40 @@ public class FindIfPathExistsInGraph1971 {
         System.out.println(validPath(6, new int[][]{{0, 1}, {0, 2}, {3, 5}, {5, 4}, {4, 3}}, 0, 5)); // false
     }
 
+    private static int find(int[] parent, int x) {
+        if (parent[x] != x) parent[x] = find(parent, parent[x]);
+        return parent[x];
+    }
+
     public static boolean validPath(int n, int[][] edges, int source, int destination) {
-        HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
         for (int[] edge : edges) {
-            graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
-            graph.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
+            int parent0 = find(parent, edge[0]);
+            int parent1 = find(parent, edge[1]);
+            if (parent0 != parent1) parent[parent0] = parent1;
         }
-        boolean[] visited = new boolean[n];
-        LinkedList<Integer> queue = new LinkedList<>();
-        visited[source] = true;
-        queue.add(source);
-        while (!queue.isEmpty()) {
-            int node = queue.remove();
-            if (node == destination) return true;
-            for (int neighbor : graph.getOrDefault(node, new ArrayList<>()))
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    queue.add(neighbor);
-                }
-        }
-        return false;
+        return find(parent, source) == find(parent, destination);
+
+//        HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
+//        for (int[] edge : edges) {
+//            graph.computeIfAbsent(edge[0], k -> new ArrayList<>()).add(edge[1]);
+//            graph.computeIfAbsent(edge[1], k -> new ArrayList<>()).add(edge[0]);
+//        }
+//        boolean[] visited = new boolean[n];
+//        LinkedList<Integer> queue = new LinkedList<>();
+//        visited[source] = true;
+//        queue.add(source);
+//        while (!queue.isEmpty()) {
+//            int node = queue.remove();
+//            if (node == destination) return true;
+//            for (int neighbor : graph.getOrDefault(node, new ArrayList<>()))
+//                if (!visited[neighbor]) {
+//                    visited[neighbor] = true;
+//                    queue.add(neighbor);
+//                }
+//        }
+//        return false;
 
 //        HashMap<Integer, ArrayList<Integer>> graph = new HashMap<>();
 //        for (int[] edge : edges) {
