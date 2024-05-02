35680be9077ee8a4a17550549c3e0a25eaf807ff
95tuanle <tlna95@icloud.com>
2024-05-02 13:16:17 -0400

    add initial Java solution

---
+++ java/LargestPositiveIntegerThatExistsWithItsNegative2441.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+/*
+Given an integer array nums that does not contain any zeros, find the largest positive integer k such that -k also exists in the array.
+Return the positive integer k. If there is no such integer, return -1.
+*/
+public class LargestPositiveIntegerThatExistsWithItsNegative2441 {
+    public static void main(String[] args) {
+        int[] nums = {3, 2, -2, 5, -3};
+        System.out.println(findMaxK(nums));
+
+        int[] nums2 = {1, 2, 3, 4, 5};
+        System.out.println(findMaxK(nums2));
+    }
+
+    public static int findMaxK(int[] nums) {
+        Arrays.sort(nums);
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] == -nums[right]) return nums[right];
+            if (nums[left] < -nums[right]) left++;
+            else right--;
+        }
+        return -1;
+    }
+}
